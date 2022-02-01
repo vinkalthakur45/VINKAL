0d92cd3f0899c03441281bdcd26b6d2ed3eddc94
vinkalthakur45 <98394570+vinkalthakur45@users.noreply.github.com>
2022-02-01 14:29:00 +0530

    Update Sum_Of_Number_Till_N.java

--- Sum_Of_Number_Till_N.java
+++ Sum_Of_Number_Till_N.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Sum_Of_Number_Till_N {
 	  public static void main(String args[]){
 	      int sum = 0;
-	      System.out.print(" ");
+	      System.out.print("");
 	      Scanner sc = new Scanner(System.in);
 	      int num = sc.nextInt();
 
