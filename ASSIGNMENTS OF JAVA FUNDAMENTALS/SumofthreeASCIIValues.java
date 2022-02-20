75b4917e14fc3f93a2a4f38bea5862eadc902831
vinkalthakur45 <98394570+vinkalthakur45@users.noreply.github.com>
2022-02-20 21:41:52 +0530

    Add files via upload

---
+++ ASSIGNMENTS OF JAVA FUNDAMENTALS/SumofthreeASCIIValues.java
@@ -0,0 +1,13 @@
+public class SumofthreeASCIIValues {
+	public static void main(String args[]) {
+		int a = 'a';
+		System.out.println("a value is " + a);
+		int a1 = 'D';
+		System.out.println("D value is " + a1);
+		int a2 = 'A';
+		System.out.println("A value is " + a2);
+		int result = a + a1 + a2;
+		System.out.println("Sum of three characters value =" + result);
+
+	}
+}
